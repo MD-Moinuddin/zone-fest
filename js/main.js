cd3e7dfdb83946a72db40c26cd5f1b54e03d2d6c
MD-Moinuddin <moinuddinmd956@gmail.com>
2023-10-11 15:42:01 +0200

    Project design done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+const toggleButton = document.getElementById('toggleButton');
+const sidebar = document.getElementById('sidebar');
+const closeButton = document.getElementById('closeButton');
 
+toggleButton.addEventListener('click', () => {
+    sidebar.classList.add('open');
+});
+
+closeButton.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+});
